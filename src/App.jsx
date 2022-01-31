28e4940f282c46a7d4800677afbd64564f8d214b
kxrxll <kovalyovkirill@gmail.com>
2022-01-31 22:10:59 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Time from './Time';
+import './bootstrap.css';
+
+function App() {
+  return (
+    <div>
+      <Time />
+    </div>
+  )
+}
+
+export default App;
